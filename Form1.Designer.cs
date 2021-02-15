2dc6728faa6a22ce0e8c7d7d4e94aea55acb1806
Sas Iacob <sasiacobconstantin@yahoo.com>
2021-02-15 23:11:59 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace kMeans
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_drawLines = new System.Windows.Forms.Button();
+            this.btn_Centroizi = new System.Windows.Forms.Button();
+            this.label_nrCentr = new System.Windows.Forms.Label();
+            this.btn_GenPct = new System.Windows.Forms.Button();
+            this.btn_Grupare = new System.Windows.Forms.Button();
+            this.btn_Centrare = new System.Windows.Forms.Button();
+            this.btn_blank = new System.Windows.Forms.Button();
+            this.CostLabel = new System.Windows.Forms.Label();
+            this.CostMicLabel = new System.Windows.Forms.Label();
+            this.OprireLabel = new System.Windows.Forms.Label();
+            this.label_coord = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(4, 30);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 738);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseHover += new System.EventHandler(this.panel1_MouseHover);
+            // 
+            // btn_drawLines
+            // 
+            this.btn_drawLines.Location = new System.Drawing.Point(889, 36);
+            this.btn_drawLines.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_drawLines.Name = "btn_drawLines";
+            this.btn_drawLines.Size = new System.Drawing.Size(100, 28);
+            this.btn_drawLines.TabIndex = 1;
+            this.btn_drawLines.Text = "Draw Lines";
+            this.btn_drawLines.UseVisualStyleBackColor = true;
+            this.btn_drawLines.Click += new System.EventHandler(this.btn_drawLines_Click);
+            // 
+            // btn_Centroizi
+            // 
+            this.btn_Centroizi.Location = new System.Drawing.Point(889, 82);
+            this.btn_Centroizi.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Centroizi.Name = "btn_Centroizi";
+            this.btn_Centroizi.Size = new System.Drawing.Size(100, 44);
+            this.btn_Centroizi.TabIndex = 2;
+            this.btn_Centroizi.Text = "Generare Centroizi";
+            this.btn_Centroizi.UseVisualStyleBackColor = true;
+            this.btn_Centroizi.Click += new System.EventHandler(this.btn_Centroizi_Click);
+            // 
+            // label_nrCentr
+            // 
+            this.label_nrCentr.AutoSize = true;
+            this.label_nrCentr.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_nrCentr.Location = new System.Drawing.Point(836, 697);
+            this.label_nrCentr.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_nrCentr.Name = "label_nrCentr";
+            this.label_nrCentr.Size = new System.Drawing.Size(138, 20);
+            this.label_nrCentr.TabIndex = 3;
+            this.label_nrCentr.Text = "Numar centroizi: ";
+            // 
+            // btn_GenPct
+            // 
+            this.btn_GenPct.Location = new System.Drawing.Point(889, 148);
+            this.btn_GenPct.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_GenPct.Name = "btn_GenPct";
+            this.btn_GenPct.Size = new System.Drawing.Size(100, 43);
+            this.btn_GenPct.TabIndex = 4;
+            this.btn_GenPct.Text = "Generare Puncte";
+            this.btn_GenPct.UseVisualStyleBackColor = true;
+            this.btn_GenPct.Click += new System.EventHandler(this.btn_GenPct_Click);
+            // 
+            // btn_Grupare
+            // 
+            this.btn_Grupare.Location = new System.Drawing.Point(889, 210);
+            this.btn_Grupare.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Grupare.Name = "btn_Grupare";
+            this.btn_Grupare.Size = new System.Drawing.Size(100, 28);
+            this.btn_Grupare.TabIndex = 5;
+            this.btn_Grupare.Text = "Grupare";
+            this.btn_Grupare.UseVisualStyleBackColor = true;
+            this.btn_Grupare.Click += new System.EventHandler(this.btn_Grupare_Click);
+            // 
+            // btn_Centrare
+            // 
+            this.btn_Centrare.Location = new System.Drawing.Point(889, 260);
+            this.btn_Centrare.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Centrare.Name = "btn_Centrare";
+            this.btn_Centrare.Size = new System.Drawing.Size(100, 28);
+            this.btn_Centrare.TabIndex = 6;
+            this.btn_Centrare.Text = "Centrare";
+            this.btn_Centrare.UseVisualStyleBackColor = true;
+            this.btn_Centrare.Click += new System.EventHandler(this.btn_Centrare_Click);
+            // 
+            // btn_blank
+            // 
+            this.btn_blank.Location = new System.Drawing.Point(889, 308);
+            this.btn_blank.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_blank.Name = "btn_blank";
+            this.btn_blank.Size = new System.Drawing.Size(100, 28);
+            this.btn_blank.TabIndex = 7;
+            this.btn_blank.Text = "Blank";
+            this.btn_blank.UseVisualStyleBackColor = true;
+            this.btn_blank.Click += new System.EventHandler(this.btn_blank_Click);
+            // 
+            // CostLabel
+            // 
+            this.CostLabel.AutoSize = true;
+            this.CostLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CostLabel.Location = new System.Drawing.Point(836, 356);
+            this.CostLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.CostLabel.Name = "CostLabel";
+            this.CostLabel.Size = new System.Drawing.Size(49, 20);
+            this.CostLabel.TabIndex = 8;
+            this.CostLabel.Text = "Cost:";
+            // 
+            // CostMicLabel
+            // 
+            this.CostMicLabel.AutoSize = true;
+            this.CostMicLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CostMicLabel.Location = new System.Drawing.Point(836, 389);
+            this.CostMicLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.CostMicLabel.Name = "CostMicLabel";
+            this.CostMicLabel.Size = new System.Drawing.Size(123, 18);
+            this.CostMicLabel.TabIndex = 9;
+            this.CostMicLabel.Text = "Cel mai mic cost:";
+            // 
+            // OprireLabel
+            // 
+            this.OprireLabel.AutoSize = true;
+            this.OprireLabel.Location = new System.Drawing.Point(843, 428);
+            this.OprireLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.OprireLabel.Name = "OprireLabel";
+            this.OprireLabel.Size = new System.Drawing.Size(48, 17);
+            this.OprireLabel.TabIndex = 10;
+            this.OprireLabel.Text = "Oprire";
+            // 
+            // label_coord
+            // 
+            this.label_coord.AutoSize = true;
+            this.label_coord.Location = new System.Drawing.Point(886, 483);
+            this.label_coord.Name = "label_coord";
+            this.label_coord.Size = new System.Drawing.Size(46, 17);
+            this.label_coord.TabIndex = 11;
+            this.label_coord.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1047, 802);
+            this.Controls.Add(this.label_coord);
+            this.Controls.Add(this.OprireLabel);
+            this.Controls.Add(this.CostMicLabel);
+            this.Controls.Add(this.CostLabel);
+            this.Controls.Add(this.btn_blank);
+            this.Controls.Add(this.btn_Centrare);
+            this.Controls.Add(this.btn_Grupare);
+            this.Controls.Add(this.btn_GenPct);
+            this.Controls.Add(this.label_nrCentr);
+            this.Controls.Add(this.btn_Centroizi);
+            this.Controls.Add(this.btn_drawLines);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_drawLines;
+        private System.Windows.Forms.Button btn_Centroizi;
+        private System.Windows.Forms.Label label_nrCentr;
+        private System.Windows.Forms.Button btn_GenPct;
+        private System.Windows.Forms.Button btn_Grupare;
+        private System.Windows.Forms.Button btn_Centrare;
+        private System.Windows.Forms.Button btn_blank;
+        private System.Windows.Forms.Label CostLabel;
+        private System.Windows.Forms.Label CostMicLabel;
+        private System.Windows.Forms.Label OprireLabel;
+        private System.Windows.Forms.Label label_coord;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace kMeans
+{
+    public partial class Form1 : Form
+    {
+
+        class Centroid
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+            public Color color { get; set; }
+
+
+            public Centroid(int x , int y , Color color)
+            {
+                this.x = x;
+                this.y = y;
+                this.color = color;
+            }
+
+        }
+        class Punct
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+           public Color color { get; set; }
+        
+            public Punct(int x, int y,  Color color)
+            {
+                this.x = x;
+                this.y = y;
+                this.color = color;
+
+            }
+        }
+
+
+        #region generareRandom
+        public static readonly Random getRandom = new Random();
+        public static int getRandomNumber(int min, int max)
+        {
+            lock (getRandom)
+            {
+                return getRandom.Next(min, max);
+            }
+        }
+
+        public static double getRandomDouble()
+        {
+            lock (getRandom)
+            {
+                return getRandom.NextDouble();
+            }
+        }
+
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int n = 10000;   //numar de puncte
+
+        #region variabileGlobale
+        Graphics g;
+        Pen p;
+        //int cost;
+        Centroid[] centroizi;
+        Centroid[] centroizi_new;
+        int cost2 = 10000000;
+        bool oprit = false;
+        Punct[] punct;
+        int[,] inputData;
+        int nr_centroizi;
+        int[,] centruGreutate;  //centruGreutate [culoare,Gx,Gy];
+        Color[] colors = { Color.Blue, Color.Red, Color.Green, Color.Brown, Color.Turquoise, Color.Orange, Color.Pink, Color.Coral, Color.LightGreen, Color.Lime };
+        #endregion
+
+        private void btn_drawLines_Click(object sender, EventArgs e)
+        {
+            g = panel1.CreateGraphics();
+            p = new Pen(Color.Black);
+            p.Width = 3;
+            g.TranslateTransform((float)(panel1.Width / 2), (float)(panel1.Height / 2));
+            drawLines();
+        }
+        private void drawLines()
+        {
+
+
+
+            g.DrawLine(p, 0, 300, 0, -300);
+            g.DrawLine(p, -300, 0, 300, 0);
+        }
+        private void GenereazaCentroizi()
+        {
+            int x, y;
+            nr_centroizi = getRandomNumber(2, 11);
+
+
+            label_nrCentr.Text = $"Numar Centroizi: {nr_centroizi}";
+            btn_Centroizi.Enabled = false;
+            centroizi = new Centroid[nr_centroizi];
+
+
+
+            for (int i = 0; i < nr_centroizi; i++)
+            {
+
+
+
+                x = getRandomNumber(-299, 300);
+                y = getRandomNumber(-299, 300);
+                centroizi[i] = new Centroid(x, y, colors[i]);
+
+                g.DrawEllipse((new Pen(centroizi[i].color, 3)), centroizi[i].x, centroizi[i].y, 10, 10);
+            }
+        }
+
+
+
+        private void btn_Centroizi_Click(object sender, EventArgs e)
+        {
+            GenereazaCentroizi();
+        }
+
+        private void btn_GenPct_Click(object sender, EventArgs e)
+        {
+            punct = new Punct[n];
+            string path = "Input.txt";
+            inputData = new int[n, 2];
+            FileStream fisier;
+
+      
+            if ((!File.Exists(path)) || (File.ReadLines(path).Count() != n))
+            {
+                fisier = File.Open(path, FileMode.Create);
+
+                StreamWriter sw = new StreamWriter(fisier);
+                for (int i = 0; i < n; i++)
+                {
+                    int x = getRandomNumber(-299, 300);
+                    int y = getRandomNumber(-299, 300);
+                    sw.WriteLine(x + " " + y);
+
+                }
+                sw.Close();
+            } // creeaza fisier daca e nevoie.
+
+
+            string[] lines = File.ReadAllLines(path);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] words = lines[i].Split(' ');
+
+
+                punct[i] = new Punct(Convert.ToInt32(words[0]), Convert.ToInt32(words[1]), colors[i % 10]); //creez punct cu toate culorile
+
+            
+                
+           
+
+
+            }   
+
+            for (int i = 0; i < n; i++)
+            {
+
+              
+                DeseneazaPunct(i);
+                    
+            }
+
+        }
+
+        private void DeseneazaPunct(int i)
+        {
+            g.FillRectangle((new SolidBrush(punct[i].color)), punct[i].x, punct[i].y, 2, 2);
+        }
+
+        private void GruparePct()
+        {
+          
+            int cost =0;
+            centruGreutate = new int[nr_centroizi, 3];
+
+            drawLines();
+            int distanta;
+           
+
+            for (int i = 0; i < n; i++)
+            {
+                int distantaFinala = 10000;
+
+                for (int j = 0; j < nr_centroizi; j++)
+                {
+                    distanta = Convert.ToInt32(Math.Sqrt(Math.Pow((punct[i].x - centroizi[j].x), 2) + Math.Pow((punct[i].y - centroizi[j].y), 2)));
+                    if (distanta < distantaFinala)
+                    {
+                        distantaFinala = distanta;
+                        punct[i].color = centroizi[j].color;
+
+
+                    }
+
+                   
+                }
+                cost += distantaFinala;
+               
+                
+
+            
+                DeseneazaPunct(i);
+            }
+            
+            if (cost2 > cost)
+            {
+                cost2 = cost;
+             
+            CostMicLabel.Text = $"Cel mai mic cost: {cost2}";
+            CostMicLabel.Update();
+
+                
+            }
+            else if(!oprit)
+            {
+                OprireLabel.Text = $"Oprire la: {cost2}";
+                OprireLabel.Update();
+                oprit = true;
+            }
+            drawLines();
+            CostLabel.Text = $"Cost: {Convert.ToInt32(cost)}";
+            CostLabel.Update();
+
+
+
+
+        }
+            private void btn_Grupare_Click(object sender, EventArgs e)
+        {
+
+            GruparePct();
+       
+        }
+
+
+        private void RecentrareCentroizi()
+        {
+            Centroid[] c = new Centroid[nr_centroizi];
+            Centroid[] c2 = new Centroid[nr_centroizi];
+            c = centroizi;
+            
+            int[,] centruG = new int[centroizi.Length, 3]; //centruG [culoare, sumX, sumY,nrPct]
+            centroizi_new = new Centroid[nr_centroizi];
+
+            for (int i = 0; i < punct.Length; i++)
+            {
+                {
+                    for (int j = 0; j < centroizi.Length; j++)
+                    {
+
+                        if (punct[i].color==centroizi[j].color)
+                        {
+                            centruG[j, 0] += punct[i].x;
+                            centruG[j, 1] += punct[i].y;
+                            centruG[j, 2]++;
+                        }
+                    }
+                }
+
+            }
+
+            for (int j = 0; j < centroizi.Length; j++)
+            {
+                g.DrawEllipse((new Pen(Color.White, 3)), centroizi[j].x, centroizi[j].y, 10, 10);
+
+                if (centruG[j, 2] != 0)
+                {
+                    
+
+                    centruG[j, 0] /= centruG[j, 2];
+                    centruG[j, 1] /= centruG[j, 2];
+
+                    centroizi_new[j] = new Centroid(centruG[j, 0], centruG[j, 1], centroizi[j].color);
+                   // centroizi[j].x = centruG[j, 0];
+                    //centroizi[j].y = centruG[j, 1];
+
+                   
+
+                }
+                else
+                {
+                    centroizi_new[j] = new Centroid(centroizi[j].x, centroizi[j].y, centroizi[j].color);
+                }
+                g.DrawEllipse((new Pen(centroizi_new[j].color, 3)), centroizi_new[j].x, centroizi_new[j].y, 10, 10);
+            }
+
+
+        }
+
+        private void btn_Centrare_Click(object sender, EventArgs e)
+        {
+
+
+
+            bool changed;
+            do
+            {
+               
+                Centroid[] centr_old = new Centroid[nr_centroizi];
+                Centroid[] centr_new = new Centroid[nr_centroizi];
+                
+               // Thread.Sleep(1000);
+                 changed = false;
+                RecentrareCentroizi();
+                centr_old = centroizi;
+
+                if (centroizi_new != null)
+                {
+                    centroizi = centroizi_new;
+                centr_new = centroizi_new;
+                }
+                GruparePct();
+
+                for (int i = 0; i < nr_centroizi; i++)
+                {
+                    if ((centr_new[i].x != centr_old[i].x)|| (centr_new[i].y != centr_old[i].y))
+                    {
+                        changed = true;
+                        break;
+                    }
+                }
+
+
+            } while (changed);
+
+           
+    }
+
+        private void btn_blank_Click(object sender, EventArgs e)
+        {
+            g.FillRectangle((new SolidBrush(Color.White)), -300, -300, 600, 600);
+            drawLines();
+            GenereazaCentroizi();
+        }
+
+        private void panel1_MouseHover(object sender, EventArgs e)
+        {
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            label_coord.Text = $" X: {e.X}  Y: {e.Y} ";
+            
+        }
+    }
+
+
+
+}
+            
+        
+
+ 
